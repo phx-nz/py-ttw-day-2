1f3ec71042d72e46223e16a21bf9ce85f004e18e
Phoenix Zerin <todofixthis@users.noreply.github.com>
2023-11-02 15:53:33 +1300

    Stretch goal for day 2 🤸

--- test/integration/api/v1/test_edit_profile.py
+++ test/integration/api/v1/test_edit_profile.py
@@ -1,8 +1,9 @@
 """
-Unit tests for PUT ``/v1/profile/{profile_id}``.
+Unit tests for ``PUT /v1/profile/{profile_id}``.
 """
 from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
+from httpx import Response
 
 from models.profile import Profile
 from services.profile import EditProfileRequest
@@ -12,7 +13,7 @@ def test_happy_path(client: TestClient, profiles: list[Profile]):
     """
     Editing a profile successfully.
     """
-    target_profile = profiles[0]
+    target_profile: Profile = profiles[0]
 
     request_body = EditProfileRequest(
         username="calmcat451",
@@ -23,18 +24,21 @@ def test_happy_path(client: TestClient, profiles: list[Profile]):
         email="ethel.chen@example.com",
     )
 
-    response = client.put(
+    response: Response = client.put(
         f"/v1/profile/{target_profile.id}",
-        json=dict(request_body),
+        json=jsonable_encoder(request_body),
     )
 
     assert response.status_code == 200
-
-    # The response will contain the profile data, with updated values.
-    expected_response = jsonable_encoder(target_profile)
-    expected_response.update(request_body)
-
-    assert response.json() == expected_response
+    assert response.json() == {
+        "id": target_profile.id,
+        "username": "calmcat451",
+        "password": "shortjane",
+        "gender": "female",
+        "full_name": "Ethel Chen",
+        "street_address": "3775 Deerswim Lane",
+        "email": "ethel.chen@example.com",
+    }
 
 
 def test_non_existent_profile(client: TestClient):
@@ -50,5 +54,7 @@ def test_non_existent_profile(client: TestClient):
         email="ethel.chen@example.com",
     )
 
-    response = client.put("/v1/profile/999", json=dict(request_body))
+    response: Response = client.put(
+        "/v1/profile/999", json=jsonable_encoder(request_body)
+    )
     assert response.status_code == 404
